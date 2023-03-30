eee32688d43a8ac8af344c04959b52d16135b62f
Ethan <enixon7@uncc.edu>
2023-03-29 20:58:43 -0400

    CSS updates

--- Interface/tailwind.config.js
+++ Interface/tailwind.config.js
@@ -2,13 +2,18 @@
 module.exports = {
   content: ["./templates/*.html"],
   theme: {
+    
     extend: {
+      fontFamily: {
+        'oswald': ['Oswald', 'sans-serif']
+      },
       colors: {
         "main-green": "#005035",
         "gold1":" #a49665",
         "darker-green": "#233a30",
         "gold2": "#ad9651",
         "main-gray": "#222",
+        "main-gold": "#a49665"
       }
     },
   },
