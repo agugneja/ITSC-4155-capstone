99e4134118eec3e0a8491b2802071577ce9f39bd
stdonnelly <80422933+stdonnelly@users.noreply.github.com>
2023-04-13 00:04:00 -0400

    CSV download

--- Model/model.py
+++ Model/model.py
@@ -19,17 +19,36 @@ faculty_members = db.facultyMembers
 # Define some functions
 
 # For each profile, upsert that profile into the database
+
+
 def update_by_name(profile_list):
     for profile in profile_list:
         # Replace the faculty member in the database with the new profile
         faculty_members.replace_one({'Title': profile['Title'], },
-                                   {'Title': profile['Title'], 'Content': str(profile['Content'])},
-                                   upsert=True)
+                                    {'Title': profile['Title'],
+                                     'Content': str(profile['Content'])},
+                                    upsert=True)
+
+# Dump entire database for the CSV
+# This is just to make sure only necessary data is gathered
+
+
+def csv_dump():
+    cursor = faculty_members.find(
+        projection={'url': True, 'name': True, 'rawHtml': True})
+
+    return [FacultyProfile(faculty_member.get('name'),
+                           faculty_member.get('rawHtml'),
+                           url=faculty_member.get('url'))
+            for faculty_member in faculty_members.find(projection={'name': True, 'rawHtml': True, 'url': True})]
 
 # Make the FacultyProfile dataclass
+
+
 @dataclass
 class FacultyProfile():
-    name: str
-    rawHtml: str
-    tel: str
-    email: str
+    name: str = None
+    rawHtml: str = None
+    tel: str = None
+    email: str = None
+    url: str = None
