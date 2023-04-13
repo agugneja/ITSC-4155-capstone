067910d3b3dc4bbfd68ba6f9bd4a2453e8d61ee5
stdonnelly <80422933+stdonnelly@users.noreply.github.com>
2023-04-13 13:28:53 -0400

    Possibly make web scraper use dataclass

--- Model/model.py
+++ Model/model.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 from . import constants
@@ -11,29 +11,36 @@ client = MongoClient(host=connect_URI, server_api=ServerApi("1"))
 client.admin.command('ping')
 print("Successfully connected to MongoDB")
 
+
+# Make the FacultyProfile dataclass
+@dataclass
+class FacultyProfile():
+    name: str
+    rawHtml: str = None
+    tel: str = None
+    email: str = None
+    url: str = None
+
+
 # Define collections
 db = client.itsc4155_team15
-
 faculty_members = db.facultyMembers
+
 
 # Define some functions
 
 # For each profile, upsert that profile into the database
+def update_by_name(profiles: list[FacultyProfile]):
 
-
-def update_by_name(profile_list):
-    for profile in profile_list:
+    for profile in profiles:
         # Replace the faculty member in the database with the new profile
-        faculty_members.replace_one({'Title': profile['Title'], },
-                                    {'Title': profile['Title'],
-                                     'Content': str(profile['Content'])},
-                                    upsert=True)
+        faculty_members.replace_one(
+            {'name': profile.name}, asdict(profile), upsert=True)
+
 
 # Dump entire database for the CSV
 # This is just to make sure only necessary data is gathered
-
-
-def csv_dump():
+def csv_dump() -> list[FacultyProfile]:
     cursor = faculty_members.find(
         projection={'url': True, 'name': True, 'rawHtml': True})
 
@@ -41,14 +48,3 @@ def csv_dump():
                            faculty_member.get('rawHtml'),
                            url=faculty_member.get('url'))
             for faculty_member in faculty_members.find(projection={'name': True, 'rawHtml': True, 'url': True})]
-
-# Make the FacultyProfile dataclass
-
-
-@dataclass
-class FacultyProfile():
-    name: str = None
-    rawHtml: str = None
-    tel: str = None
-    email: str = None
-    url: str = None
