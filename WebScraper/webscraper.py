084d5931a8fb11c1530a9cc1d52aeff5f4ffd28f
Ethan <enixon7@uncc.edu>
2023-04-17 16:22:29 -0400

    Slight changes to accomodate dataclass

--- WebScraper/webscraper.py
+++ WebScraper/webscraper.py
@@ -6,6 +6,7 @@ from . import Engineering
 from . import CHHS
 from . import LiberalScience
 from . import Misc
+from .LiberalScience import English
 from Model.model import FacultyProfile, update_by_name
 import importlib
 from concurrent.futures import ThreadPoolExecutor
@@ -19,8 +20,7 @@ def is_url_valid(url: str) -> bool:
     return True
 
 def main():
-    # packages = [CHHS, Education, Engineering, LiberalScience, Misc]
-    packages = [CHHS]
+    packages = [CHHS, Education, Engineering, LiberalScience, Misc]
     departments = []
     # For each folder containing modules
     for package in packages:
@@ -34,24 +34,10 @@ def main():
     with ThreadPoolExecutor(max_workers=15) as p:
         department_objects = list(p.map(lambda x: x(), departments))
 
-    # profileList = [d.profilePages if 'profilePages' in d.__dict__ else d.profiles for d in department_objects]  # List for Profile Page Content
-    # profileList = list(itertools.chain.from_iterable(profileList))
-    # urlList = [d.facultyURLs for d in department_objects]  # List for URLs to the faculty profiles
-
-    # urlList = list(itertools.chain.from_iterable(urlList))
-    profile_list = []
-    url_list = []
+    profiles = []
     for d in department_objects:
-        url_list += d.facultyURLs
-        profile_list += d.profiles
-    
-    bad_urls = []
-    # with ThreadPoolExecutor(max_workers=25) as p:
-    #     bad_urls_iterator = p.map(lambda x: x if not is_url_valid(x) else '', url_list)
-    #     bad_urls = [url for url in bad_urls_iterator if url != '']
-
-    # url_list = [(url if url not in bad_urls else '') for url in url_list ]
-            
+        profiles += d.profiles
+                
 
     # site, type, action, title, excerpt, content, date, author, slug, status, menu-order, password, categories, tags, taxonomy-{name}, meta-{name}
     header = ["site", "type", "action", "title", "excerpt", "content", "date", "author", "slug", "status", "menu-order", "password",
@@ -62,18 +48,11 @@ def main():
         writer.writerow(header)
 
         # Writes to CSV based on specfic formatting defined in header list
-        for i in range(len(profile_list)):
-            listy = [url_list[i], "post", "update", profile_list[i]
-                    ["Title"], "", profile_list[i]["Content"], "", "", "", "", ""]
-            writer.writerow(listy)
-    
-    # I know this is bad practice.
-    # I will change this when web scrapers just use FacultyProfile instead of to janky parallel arrays.
-    # This is also untested because my internet is slow.
-    faculty_profiles = []
-    for i in range(len(profile_list)): # 💩
-        faculty_profiles.append(FacultyProfile(name=profile_list[i]['Title'], rawHtml=str(profile_list[i]["Content"]), url=url_list[i]))
-    update_by_name(faculty_profiles)
+        for profile in profiles:
+            row = [profile.url, "post", "update", profile.name, "", profile.rawHtml, "", "", "", "", ""]
+            writer.writerow(row)
+
+    update_by_name(profiles)
 
 if __name__ == '__main__':
     main()
