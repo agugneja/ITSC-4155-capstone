379552c946a0f203863a8be9101cffcc4aa053ff
Ethan <enixon7@uncc.edu>
2023-04-27 18:02:21 -0400

    Added email scraping

--- WebScraper/FacultyWebScraper.py
+++ WebScraper/FacultyWebScraper.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup, Tag
 from bs4 import ResultSet
 import requests
+import re
 from typing import Optional
 from Model.model import FacultyProfile
 from abc import ABC, abstractmethod
@@ -55,7 +56,8 @@ class FacultyWebScraper(ABC):
                 try:
                     rawHtml = self.getRawHtml(soup, url)
                     name = self.getName(soup, url)
-                    profiles.append(FacultyProfile(name=name, department=self.__class__.__name__, rawHtml=rawHtml, url=url))
+                    email = self.getEmail(soup, rawHtml, url)
+                    profiles.append(FacultyProfile(name=name, department=self.__class__.__name__, rawHtml=rawHtml, url=url, email=email))
                 except Exception as e:
                     print(f"Something went wrong when visiting {url}:")
                     print(e)
@@ -80,6 +82,51 @@ class FacultyWebScraper(ABC):
             print(e)
             # self.bad_urls.append(url)
 
+    def getEmail(self, entire_html: Tag, profile_html: Optional[Tag], url: str) -> Optional[str]:
+        """Finds a single uncc email address from the page given
+        
+        First tries lookin on whole page. If no emails were found return None
+        If just one email was found, return that email.
+        If more than 1 email was found in the whole page, 
+        check just the profile with the same logic
+
+        ### Args:
+            `entire_html (Tag)`: HTML for the entire page being scraped
+            `profile_html (Optional[Tag])`: HTML for just the profile section of the page being scraped
+            `url (str)`: The url of the page being scraped
+
+        ### Returns:
+            Optional[str]: the email found if one was found, else None
+        """
+        for html in [entire_html, profile_html]:
+            if html is not None:
+                emails = self._findEmails(html)
+                if len(emails) == 0:
+                    print(f'no emails found for {url}')
+                    return None
+                elif len(emails) == 1:
+                    return emails.pop()
+                if html == profile_html:
+                    print(f'more than 1 email found for {url}')
+            
+        return None
+
+    def _findEmails(self, html: Tag) -> set[str]:
+        """Finds all unique uncc email addresses contained in the html
+
+        ### Args:
+            `html (Tag)`: The html to check for email addresses 
+
+        ### Returns:
+            `set[str]`: The unique email addresses found
+        """
+        if emails := html.find_all(text=re.compile(r'[a-zA-Z0-9._%+-]+@uncc\.edu')):
+            # no easy way to use capture groups in the above section, so just do that here
+            emails = [re.search(r'([a-zA-Z0-9._%+-]+@uncc\.edu)', email).group(1) for email in emails]
+            unique_emails = {email.lower() for email in emails}
+            return unique_emails
+        return set()
+        
     @abstractmethod
     def getRawHtml(self, soup: BeautifulSoup, url: str) -> Optional[Tag]:
         """Scrapes the profile content from a faculty profile page
